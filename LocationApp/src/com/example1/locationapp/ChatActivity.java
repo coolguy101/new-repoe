65efdce2731a54afdd03f101c4ea00b4970664f6
coolguy101 <jiang_501@hotmail.com>
2014-05-07 11:16:40 -0600

    login in done

--- LocationApp/src/com/example1/locationapp/ChatActivity.java
+++ LocationApp/src/com/example1/locationapp/ChatActivity.java
@@ -1,20 +1,59 @@
 package com.example1.locationapp;
 
+import java.util.ArrayList;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SmackAndroid;
+import org.jivesoftware.smack.XMPPConnection;
+
 import Model.Comments;
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ChatActivity extends Activity {
 	private Comments comment;
+	private ListView ChatListView;
+	private ArrayAdapter<String> ListViewAdapter;
+	private ArrayList<String> ListViewItem;
+	private XMPPConnection connection;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat);
 		comment = (Comments) getIntent().getSerializableExtra("comments");
-		
-
+		ChatListView = (ListView) findViewById(R.id.listView1);
+		ListViewItem = new ArrayList<String>();
+		ListViewAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,ListViewItem);
+		ChatListView.setAdapter(ListViewAdapter);
+		Context context = this;
+		SmackAndroid.init(context);
+		ConnectionConfiguration config = new ConnectionConfiguration("54.186.214.150", 5222);
+        connection = new XMPPConnection(config);
+        new AsyncTask<Void, Void, Void>()
+        {
+        
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			try{
+		        connection.connect();
+		        connection.login("yazhou1","123456");
+		        }
+		        catch(Exception e)
+		        {
+		        	System.out.println("exception is:"+e.toString());
+		        }
+			return null;
+		}
+        }.execute();
 	}
 
 	@Override
